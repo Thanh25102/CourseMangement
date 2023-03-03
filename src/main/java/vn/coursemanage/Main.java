fd7495d99fd494f1ba44dcaefbe6433edc3c187b
phuthinh <thinhcp@gshop.com.vn>
2023-03-04 00:16:42 +0700

    add 3 gui

--- src/main/java/vn/coursemanage/Main.java
+++ src/main/java/vn/coursemanage/Main.java
@@ -1,13 +1,16 @@
 package vn.coursemanage;
 
 import vn.coursemanage.dao.PersonDao;
+import vn.coursemanage.gui.MainGUI;
 import vn.coursemanage.model.Person;
 
 public class Main {
     public static void main(String[] args) {
-        PersonDao repo = new PersonDao();
-        repo.findPersonByField("firstName","u")
-                .forEach(System.out::println);
-        repo.findPerson().forEach(System.out::println);
+        MainGUI gui = new MainGUI();
+        gui.setVisible(true);
+//        PersonDao repo = new PersonDao();
+//        repo.findPersonByField("firstName","u")
+//                .forEach(System.out::println);
+//        repo.findPerson().forEach(System.out::println);
     }
 }
